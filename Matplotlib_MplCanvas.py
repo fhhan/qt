a6ed2f8cdf5424141da871e64a241ffd4c16b871
fhhan <f.han@outlook.com>
2019-12-01 00:19:01 +0800

    add annotation

--- Matplotlib_MplCanvas.py
+++ Matplotlib_MplCanvas.py
@@ -14,7 +14,7 @@ class MplCanvas(FigureCanvas):
 		super(MplCanvas,self).__init__(self.fig)
 		
 		self.ax = self.fig.add_subplot(1,1,1)
-		#FigureCanvas.__init__(self, fig)
+		#FigureCanvas.__init__(self, fig)  应该与super(MplCanvas,self).__init__(self.fig)一样，老式写法
 
 
 class MyPlot(QWidget):
@@ -40,6 +40,7 @@ class MyPlot(QWidget):
 
 
 	def __plot(self):
+		#clear 画布，刷新画布
 		self.canvas.ax.clear()
 		x = np.linspace(-2*np.pi,2*np.pi,100)
 
@@ -49,6 +50,8 @@ class MyPlot(QWidget):
 			self.canvas.ax.plot(x,np.cos(x),'r:',label="Cos",linewidth=2)
 
 		self.canvas.ax.legend(loc=3)
+		#canvas.draw() 与 canvas.draw_idle()
+		#在 win 上canvas.draw可以，但 mac 上无反应，需要切换窗口或者最大化窗口才刷新
 		#self.canvas.draw()
 		self.canvas.draw_idle()
 		self.flag = not self.flag
